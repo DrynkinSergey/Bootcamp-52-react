711c607367057374704027f1fc0f76f9214016b3
Sergey <sergsmile29@gmail.com>
2023-06-02 10:48:17 +0300

    createReducer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import Counter from './components/Counter/Counter'
 import TodoList from './components/TodoList/TodoList'
 
 function App() {
+	// const a = [1, 2, 3]
+	// console.log('a =>>>>', a)
+	// a.push(4)
+	// console.log(a)
+
+	// const b = a
+	// console.log('b =>>>>', b)
+	// b.push(4)
+	// console.log('b.push =>>>>', b)
+	// console.log('a =>>>>', a)
+	// const newB = [...b, 4]
+	// console.log(newB)
+	// console.log(b)
+	// const a = [{ name: 'alex' }, { name: 'sergey' }]
+
+	// const el = a.find(item => item.name === 'alex')
+	// el.name = 'Olena'
+	// console.log(a)
+
 	return (
 		<div>
 			<TodoList />
--- src/redux/Todo/reducer.js
+++ src/redux/Todo/reducer.js
@@ -1,3 +1,4 @@
+import { createReducer } from '@reduxjs/toolkit'
 import { addTodo, deleteTodo, setFilter, toggleTodo } from './actions'
 
 const initialState = {
@@ -5,37 +6,53 @@ const initialState = {
 	filter: 'All',
 }
 
-export const todoReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case addTodo.type: {
-			return {
-				...state,
-				todos: [...state.todos, action.payload],
-			}
-		}
-		case deleteTodo.type: {
-			return {
-				...state,
-				todos: state.todos.filter(todo => todo.id !== action.payload),
-			}
-		}
-		case toggleTodo.type: {
-			return {
-				...state,
-				todos: state.todos.map(todo =>
-					todo.id === action.payload
-						? { ...todo, completed: !todo.completed }
-						: todo
-				),
-			}
-		}
-		case setFilter.type: {
-			return {
-				...state,
-				filter: action.payload,
-			}
-		}
-		default:
-			return state
-	}
-}
+export const todoReducer = createReducer(initialState, {
+	[addTodo]: (state, { payload }) => {
+		state.todos.push(payload)
+	},
+	[deleteTodo]: (state, { payload }) => {
+		const todoIndex = state.todos.findIndex(todo => todo.id === payload)
+		state.todos.splice(todoIndex, 1)
+	},
+	[toggleTodo]: (state, { payload }) => {
+		const todo = state.todos.find(todo => todo.id === payload)
+		todo.completed = !todo.completed
+	},
+	[setFilter]: (state, { payload }) => {
+		state.filter = payload
+	},
+})
+// export const todoReducer = (state = initialState, action) => {
+// 	switch (action.type) {
+// 		case addTodo.type: {
+// 			return {
+// 				...state,
+// 				todos: [...state.todos, action.payload],
+// 			}
+// 		}
+// 		case deleteTodo.type: {
+// 			return {
+// 				...state,
+// 				todos: state.todos.filter(todo => todo.id !== action.payload),
+// 			}
+// 		}
+// 		case toggleTodo.type: {
+// 			return {
+// 				...state,
+// 				todos: state.todos.map(todo =>
+// 					todo.id === action.payload
+// 						? { ...todo, completed: !todo.completed }
+// 						: todo
+// 				),
+// 			}
+// 		}
+// 		case setFilter.type: {
+// 			return {
+// 				...state,
+// 				filter: action.payload,
+// 			}
+// 		}
+// 		default:
+// 			return state
+// 	}
+// }

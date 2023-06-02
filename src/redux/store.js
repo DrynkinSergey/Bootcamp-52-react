0a8b10cc612833bc7ff6b72fcaf646adaacd4fe9
Sergey <sergsmile29@gmail.com>
2023-06-02 12:52:11 +0300

    redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,38 @@
-// import { combineReducers, createStore } from 'redux'
-import { countReducer } from './Counter/counterSlice'
-// import { devToolsEnhancer } from '@redux-devtools/extension'
 import { configureStore } from '@reduxjs/toolkit'
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+import { countReducer } from './Counter/counterSlice'
 import { todoReducer } from './Todo/todoSlice'
 import { filterReducer } from './filterSlice'
-// const enhancer = devToolsEnhancer()
 
-// const rootReducer = combineReducers({
-// 	counter: countReducer,
-// 	tasks: todoReducer,
-// })
-// export const store = createStore(rootReducer, enhancer)
+const persistConfig = {
+	key: 'root',
+	version: 1,
+	storage,
+}
+const persistedReducer = persistReducer(persistConfig, todoReducer)
 export const store = configureStore({
 	reducer: {
 		counter: countReducer,
-		tasks: todoReducer,
+		tasks: persistedReducer,
 		filter: filterReducer,
 	},
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 })
+
+export const persistor = persistStore(store)
